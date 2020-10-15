46b244008ffb66a7464bd494c35d64d601150715
Ilasigma <71940889+Ilasigma@users.noreply.github.com>
2020-10-15 20:41:45 +0530

    Create index.php

---
+++ assignment-3/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment-3</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <td colspan ="4"><input type="text" class="input-cal" name="calc" placeholder="  Calculator"></td>
+        </tr>
+        <tr class="firstrow">
+            <td><input class="btn" type="button" name="AC" value="AC"></td>
+            <td><input class="btn" type="button" name="+/-" value="+/-"></td>
+            <td><input class="btn" type="button" value="√"></td>
+            <td><input class="btn" type="button" name="/" value="/"></td>
+        </tr>
+        <tr>
+            <td class="global"><input class="btn-white" type="button" name="7" value="7"></td>
+            <td class="global"><input class="btn-white" type="button" name="8" value="8"></td>
+            <td class="global"><input class="btn-white" type="button" name="9" value="9"></td>
+            <td class="globalone"><input class="btn" type="button" name="*" value="*"></td>
+        </tr>
+        <tr>
+            <td class="global"><input class="btn-white" type="button" name="" value="4"></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="5"></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="6"></td>
+            <td class="globalone"><input class="btn" type="button" name="" value="-"></td>
+        </tr>
+        <tr>
+            <td class="global"><input class="btn-white" type="button" name="" value="1"></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="2"></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="3"></td>
+            <td class="globalone"><input class="btn" type="button" name="" value="+"></td>
+        </tr>
+        <tr>
+            <td class="global"><input class="btn-white" type="button" name="" value="0"></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="."></td>
+            <td class="global"><input class="btn-white" type="button" name="" value="%"></td>
+            <td id="one"><input class="btn-white" type="button" name="" value="="></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
