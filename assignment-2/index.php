2a7fadc463672dcade39d304b9ecd794a0cb3ac2
Ilasigma <71940889+Ilasigma@users.noreply.github.com>
2020-10-15 20:35:11 +0530

    Create index.php

---
+++ assignment-2/index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assignment-2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+    <img class="header" src="images/header.jpg" alt="header">
+</header>
+<img class="heading" src="images/1heading.jpg" alt="1heading">
+<div class="row">
+    <div class="column">
+        <img src="images/1.jpg" alt="Mobile">
+        <p>Mobile</p>
+    </div>
+    <div class="column">
+        <img src="images/2.jpg" alt="Mixer">
+        <p>Mixer</p>
+    </div>
+    <div class="column">
+        <img src="images/3.jpg" alt="Laptop">
+        <p>Laptop</p>
+    </div>
+    <div class="column">
+        <img src="images/4.jpg" alt="Air Conditioner">
+        <p>Air Conditioner</p>
+    </div>
+    <div class="column">
+        <img src="images/5.jpg" alt="4G Phone">
+        <p>4G Phone</p>
+    </div>
+    <div class="column">
+        <img src="images/6.jpg" alt="Automotive">
+        <p>Automotive</p>
+    </div>
+</div>
+<img class="heading" src="images/2heading.jpg" alt="2heading">
+<div class="row">
+    <div class="column">
+        <img src="images/7.jpg" alt="Travel">
+        <p>Travel</p>
+    </div>
+    <div class="column">
+        <img src="images/8.jpg" alt="Sarees">
+        <p>Sarees</p>
+    </div>
+    <div class="column">
+        <img src="images/9.jpg" alt="Footwear">
+        <p>Footwear</p>
+    </div>
+    <div class="column">
+        <img src="images/10.jpg" alt="Kid's">
+        <p>Kid's</p>
+    </div>
+    <div class="column">
+        <img src="images/11.jpg" alt="Dress">
+        <p>Dress</p>
+    </div>
+    <div class="column">
+        <img src="images/12.jpg" alt="Product">
+        <p>Product</p>
+    </div>
+</div>
+<img class="heading" src="images/3heading.jpg" alt="3heading">
+<div class="row">
+    <div class="column">
+        <img src="images/13.jpg" alt="Dineer Set">
+        <p>Dineer Set</p>
+    </div>
+    <div class="column">
+        <img src="images/14.jpg" alt="Bedsheet">
+        <p>Bedsheet</p>
+    </div>
+    <div class="column">
+        <img src="images/15.jpg" alt="Cloth Stands">
+        <p>Cloth Stands</p>
+    </div>
+    <div class="column">
+        <img src="images/16.jpg" alt="Tools">
+        <p>Tools</p>
+    </div>
+    <div class="column">
+        <img src="images/17.jpg" alt="Notebooks">
+        <p>Notebooks</p>
+    </div>
+    <div class="column">
+        <img src="images/18.jpg" alt="Kitchen Tools">
+        <p>Kitchen Tools</p>
+    </div>
+</div>
+<footer>
+    <img class="footer" src="images/footer.jpg" alt="footer.jpg">
+</footer>
+
+</body>
+</html>
