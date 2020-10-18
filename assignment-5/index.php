d7fcdfa6789cd90250c1f8529f71e7610d5a0a60
Ilasigma <71940889+Ilasigma@users.noreply.github.com>
2020-10-19 00:44:06 +0530

    index.php

---
+++ assignment-5/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="bgimg-1">
+    <div class="header">
+        <button type="button">Home</button>
+        <button type="button">Gallery</button>
+        <button type="button">Reservation</button>
+    </div>
+    <div class="caption">
+        <div class="center">
+            <span class="border">THE</span>
+            <span class="border">SPACE</span>
+            <span class="border">STATION</span>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="bgimg">
+        <span>Gallery</span>
+    </div>
+    <div class="row">
+        <div class="column">
+            <img class="zoom" src="images/thumb/thumbnail_1.jpg" alt="thumbnail_1">
+            <img class="zoom" src="images/thumb/thumbnail_2.jpg" alt="thumbnail_2">
+            <img class="zoom" src="images/thumb/thumbnail_3.jpg" alt="thumbnail_3">
+            <img class="zoom" src="images/thumb/thumbnail_4.jpg" alt="thumbnail_4">
+        </div>
+        <div class="column">
+            <img class="zoom" src="images/thumb/thumbnail_5.jpg" alt="thumbnail_5">
+            <img class="zoom" src="images/thumb/thumbnail_6.jpg" alt="thumbnail_6">
+            <img class="zoom" src="images/thumb/thumbnail_7.jpg" alt="thumbnail_7">
+            <img class="zoom" src="images/thumb/thumbnail_8.jpg" alt="thumbnail_8">
+        </div>
+        <div class="column">
+            <img class="zoom" src="images/thumb/thumbnail_9.jpg" alt="thumbnail_9">
+        </div>
+    </div>
+
+    <div class="reserve-main">
+
+        <div class="bgimg">
+            <span>Reservations</span>
+        </div>
+
+        <div class="reserve">
+            <div class="reserve-title"><span class="font-size">Y</span>OUR ADVENTURE IS JUST AROUND THE MOON.</div>
+            <p>Reserve your flight today!</p>
+            <p>Please provide us with your basic contact information.</p>
+            <div class="reserve1">
+                <form class="info">
+                    <div><input type="text" name="First Name" placeholder="First Name">
+                        <input type="text" name="Last Name" placeholder="Last Name">
+                        <input type="text" name="Email Address" placeholder="Email Address">
+                    </div>
+                    <div class="info1"><input type="text" name="Phone number" placeholder="phone Number"></div>
+                    <div><input class="info2" type="submit" value="Submit">
+                        <span id="terms">Terms and conditions</span></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="footer">www.thespacestation.space | © 2019</div>
+</footer>
+</body>
+</html>
