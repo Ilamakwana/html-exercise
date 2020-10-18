d239e664bdfa76d525878f71256bf7eaa706b7ee
Ilasigma <71940889+Ilasigma@users.noreply.github.com>
2020-10-18 22:43:20 +0530

    dropdown menu using CSS

---
+++ assignment-4/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment-4</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="topnav" id="myTopnav">
+    <a href="#home">Home</a>
+    <a href="#news">About</a>
+    <div class="dropdown">
+        <button class="dropbtn">Portfolio</button>
+        <div class="dropdown-content">
+            <a href="#">Page 1</a>
+            <a href="#">Page 2</a>
+            <a href="#">Page 3</a>
+            <a href="#">Page 4</a>
+        </div>
+    </div>
+    <a href="#about">Contact</a>
+</div>
+</body>
+</html>
