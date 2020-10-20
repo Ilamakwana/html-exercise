9bddeb608aec578a8ec02a6c3902de162e742f6c
Ilasigma <71940889+Ilasigma@users.noreply.github.com>
2020-10-20 22:03:13 +0530

    Delete photo.js

--- JS-Assignment/js-assignment/photo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.preview').hover(function () {
-        // console.log("hi");
-        $('#image').css("background-image","url("+ this.src+ ")");
-        $('#image').text(this.alt);
-
-    });
-});
